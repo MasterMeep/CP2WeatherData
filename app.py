b2c8c0ddb307af77e19bde83319c58b92bfa3698
MasterMeep <51376656+MasterMeep@users.noreply.github.com>
2022-04-26 09:29:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+"""
 import streamlit as st
 from datetime import date
 from plotly import graph_objs as go
@@ -20,4 +20,31 @@ selected_city = st.selectbox("Select a city", data)
 loaded = load_data(selected_city)['main']
 st.write(loaded)
 st.session_state.ls[selected_city] = loaded
-st.table(st.session_state.ls)
+st.table(st.session_state.ls)"""
+import streamlit as st
+import requests
+import json
+
+sts = st.session_state
+
+if 'ls' not in sts:
+	sts.craftRecipies = {'thing1': ['thing1', 'thing2', 'thing3']}
+	sts.refineRecipies = {}
+	sts.cityCaftBonus = {}
+	sts.cityRefineBonus = {}
+	sts.cities = ['Bridgewatch', 'Caerleon', 'Fort Sterling', 'Lymhurst', 'Thetford']
+	sts.saveCraft = {}
+	sts.saveRefine = {}
+	sts.craftItems = list(sts.craftRecipies.keys())
+	sts.refineItems = list(sts.refineRecipies.keys())
+
+selected_city = st.selectbox("Select A City", sts.cities)
+selected_item = st.selectbox('Select An Item To Craft', sts.craftItems)
+with st.form(key='columns_in_form'):
+    cols = st.beta_columns(len(sts.craftRecipies[selected_item]))
+    for i, col in enumerate(cols):
+        col.number_input(f"Enter price of {sts.craftRecipies[selected_item][i]}", key=i)
+    submitted = st.form_submit_button('Submit')
+
+
+selected_item = st.selectbox('Select An Item To Refine', sts.refineItems)
